55263a91b0e58e56e6ff71a256807afc520829cf
sonic0828 <sonichuang@sonichuang-PC0.tencent.com>
2013-08-06 18:27:45 +0800

    页面细节修改

---
+++ trunk/js/comm.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2013, Tgideas.qq.com
+ * Creator: Sonic
+ * Date: 2013-08-06
+ * Modify: 2013-08-06
+ */
+
+$(document).ready(function(){
+	/* table tr hover
+	----------------------------------*/
+	if($(".m-table-data")){
+			
+
+	}
+
+
+})
