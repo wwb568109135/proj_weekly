015ae57e7cf572c2ecc717fd066c3409e09bed3a
Sonic0828 <sonic0828@gmail.com>
2013-08-09 10:42:32 +0800

    静态页面细节修改

--- trunk/js/comm.js
+++ trunk/js/comm.js
@@ -9,9 +9,24 @@ $(document).ready(function(){
 	/* table tr hover
 	----------------------------------*/
 	if($(".m-table-data")){
-			
+		$(".m-table-data tr").hover(function(){
+			$(this).addClass("table-hover");
+		},function(){
+			$(this).removeClass("table-hover");
+		})
 
 	}
 
+	/* btn-append-task
+	----------------------------------*/
+	if($("#btnAppendTask")){
+		$("#btnAppendTask").click(function(){
+			var $taskCreateTable = $("#taskCreateTable");
+			var appendHtml = $("#taskCreateTable tbody tr:last-child(callback)").html(),
+				appendHtmlPro = "<tr>"+ appendHtml +"</tr>";
+			$("#taskCreateTable tbody").append(appendHtmlPro);
+		})
 
+
+	}
 })
