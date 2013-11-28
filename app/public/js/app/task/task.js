e4ef043fa249ceacbfd5bb4a316d068b5392eb30
sonichuang <sonic0828@gmail.com>
2013-11-29 00:28:28 +0800

    需求列表页增加用户英文名输入自动完成

--- app/public/js/app/task/task.js
+++ app/public/js/app/task/task.js
@@ -29,6 +29,14 @@
     }else if(o.attr("data-name") == "online_date" || o.attr("data-name") == "rb_star_date" || o.attr("data-name") == "rb_end_date"){
       //- 设置日期input 反馈回的HTML
       var cbHtml = '<input type="txt" class="m-input date-picker editinput" value="'+ cbVal +'" />';
+    }else if(o.attr("data-name") == "pp"){
+      var randomNum = Math.floor((Math.random()*10000)),
+          inputId = "pp" + randomNum + "Value",
+          inputId2 = "pp" + randomNum;
+
+      var cbHtml = '<input id="'+ inputId +'" type="txt" class="m-input editinput" value="'+ cbVal +'" onfocus="_tcc_write_userscript(this.id.replace(\'Value\',\'\'), \'1\');" />';
+          cbHtml+= '<input id='+ inputId2 +' type="hidden" value="" class="editinput" userchooser="true" />';
+      console.log(cbHtml);
     }else{
       //- 设置其它表单项时反馈回的HTML
       var cbHtml = '<input type="txt" class="m-input editinput" value="'+ cbVal +'" />';
