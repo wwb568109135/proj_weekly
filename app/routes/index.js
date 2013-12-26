fe81b4bd90ad27495412696a7940e598cd05ea84
sonic0828 <sonic0828@gmail.com>
2013-12-26 09:12:34 +0800

    导出列表按需求状态进行排序

--- app/routes/index.js
+++ app/routes/index.js
@@ -47,9 +47,9 @@ exports.logout = function(req, res){
  */
 exports.task = function(req, res){
   // 公司环境，直接取OA用户名
-  // var staffName = req.cookies.user.rtx;
+  var staffName = req.cookies.user.rtx;
   // 在家环境，模拟用户名
-  var staffName = "sonichuang";
+  // var staffName = "sonichuang";
 
   if(staffName){
     Staff.find({name:staffName}).limit(1).exec(function(err,docs){
@@ -89,9 +89,9 @@ exports.task_pd = function(req, res){
       status = (req.query.status) ? parseInt(req.query.status) : {'$exists': true},
       priority = (req.query.priority) ? parseInt(req.query.priority) : {'$exists': true},
       // 公司环境，直接取OA用户名
-      // staffName = req.cookies.user.rtx,
+      staffName = req.cookies.user.rtx,
       // 在家环境，模拟用户名
-      staffName = "sonichuang",
+      // staffName = "sonichuang",
       ppQuery = {$regex: new RegExp(staffName.toLowerCase() + "\\b", "i") };
 
   // 1.把ProjectName全部取出来
@@ -129,9 +129,9 @@ exports.task_rb = function(req, res){
       status = (req.query.status) ? parseInt(req.query.status) : {'$exists': true},
       priority = (req.query.priority) ? parseInt(req.query.priority) : {'$exists': true},
       // 公司环境，直接取OA用户名
-      // staffName = req.cookies.user.rtx,
+      staffName = req.cookies.user.rtx,
       // 在家环境，模拟用户名
-      staffName = "sonichuang",
+      // staffName = "sonichuang",
       ppQuery = {$regex: new RegExp(staffName.toLowerCase() + "\\b", "i") };
 
   // 1.把ProjectName全部取出来
@@ -332,9 +332,9 @@ exports.task_detail = function(req, res) {
   } else {
       
     // 公司环境，直接取OA用户名
-    // var staffName = req.cookies.user.rtx;
+    var staffName = req.cookies.user.rtx;
     // 在家环境，模拟用户名
-    var staffName = "sonichuang";
+    // var staffName = "sonichuang";
     if(staffName){
       // 1.取出角色名
       Staff.find({name:staffName}).limit(1).exec(function(err,docs){
@@ -578,9 +578,9 @@ exports.task_callJSON = function(req, res){
   // console.log(role);
   var date = new Date(), d = date.getDate(),m = date.getMonth(),y = date.getFullYear(),
       // 公司环境，直接取OA用户名
-      // staffName = req.cookies.user.rtx,
+      staffName = req.cookies.user.rtx,
       // 在家环境，模拟用户名
-      staffName = "sonichuang",
+      // staffName = "sonichuang",
       ppQuery = {$regex: new RegExp(staffName.toLowerCase() + "\\b", "i") };
 
 
@@ -619,9 +619,9 @@ exports.task_export = function(req, res){
   var taskStarDate = (req.query.taskStarDate) ? req.query.taskStarDate : {'$exists': true},
       taskEndDate = (req.query.taskEndDate) ? req.query.taskEndDate : {'$exists': true},
       // 公司环境，直接取OA用户名
-      // staffName = req.cookies.user.rtx,
+      staffName = req.cookies.user.rtx,
       // 在家环境，模拟用户名
-      staffName = "sonichuang",
+      // staffName = "sonichuang",
       ppQuery = {$regex: new RegExp(staffName.toLowerCase() + "\\b", "i") };
 
   if(staffName){
@@ -649,7 +649,7 @@ exports.task_export = function(req, res){
                 // 3.需求筛选
                 Weekly.find({
                   $nor:[{hidden: true}], "pp":ppQuery, "rb_star_date": {"$gte": taskStarDate, "$lte": taskEndDate}
-                }).sort({progress: 1}).exec(function(err,docs){  //结果倒叙排列
+                }).sort({status: -1}).exec(function(err,docs){  //结果倒叙排列
                   res.render('export', {docs:docs})
                 })
 
