36ec1313a49628a95643e9163f7af4e101891213
sonic0828 <sonic0828@gmail.com>
2013-12-30 09:41:05 +0800

    CP更新 - 编辑页增加编辑器

---
+++ app/public/js/editor/editor.js
@@ -0,0 +1,8 @@
+var myeditor = TFL.editor({"id":"taskDescription","width":"630px","height":"300px","name":"task[content]"});
+
+//附件添加按钮
+/*
+$('.task-edit-excerpt li.add').click(function(){
+	$
+})
+*/
